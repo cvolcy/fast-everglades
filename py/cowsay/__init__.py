33811646ac547dddddd2792a535d338408a97b95
Clervens Volcy <clervens.volcy@gmail.com>
2019-02-27 20:29:52 -0500

    Add cowsay python function

---
+++ py/cowsay/__init__.py
@@ -0,0 +1,24 @@
+import random
+import logging
+import azure.functions as func
+from cowpy import cow
+
+DEFAULT_MSG = 'Hello World, This is Zeit-My-App.\nYou can update this message by setting\nthe query string parameter \'message\'.'
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    msg = cow.Cowacter(eyes=random.choice(list(cow.eye_options()))).milk(get_message(req))
+
+    return func.HttpResponse(msg, status_code=200)
+
+def get_message(req):
+    message = req.params.get('message')
+    if not message:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            message = req_body.get('message')
+    return message if message else DEFAULT_MSG
