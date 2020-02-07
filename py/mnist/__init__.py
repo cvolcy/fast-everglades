cce3b142f00aaf5802af0c2ecfa6a28890aa3d82
cvolcy <clervens.volcy@gmail.com>
2020-02-07 10:24:28 -0500

    Update mnist/__init.py__ (#7)
    Use snake_case for functions and variables

--- py/mnist/__init__.py
+++ py/mnist/__init__.py
@@ -6,13 +6,13 @@ import numpy as np
 from azure.storage.file import FileService
 from datetime import datetime, timezone
 
-inferenceSession = None
+inference_session = None
 
 FILE_ACCOUNT_NAME = os.getenv('FILE_ACCOUNT_NAME')
 FILE_ACCOUNT_KEY = os.getenv('FILE_ACCOUNT_KEY')
 
 def main(req: func.HttpRequest, context: func.Context)  -> func.HttpResponse:
-    sess = getInferenceSession()
+    sess = get_inference_session()
 
     input_name = sess.get_inputs()[0].name
     input_shape = sess.get_inputs()[0].shape
@@ -30,18 +30,18 @@ def main(req: func.HttpRequest, context: func.Context)  -> func.HttpResponse:
 
     return func.HttpResponse(json.dumps({ 'input_name': input_name, 'input_shape': input_shape[1:], 'output_name': label_name, 'prediction': data, 'createdAt': datetime.now(timezone.utc).astimezone().isoformat() }, indent=4), status_code=200)
 
-def getInferenceSession():
+def get_inference_session():
     return _initialize()
 
 def _initialize():
-    global inferenceSession
+    global inference_session
 
-    if inferenceSession is None:
+    if inference_session is None:
         file_service = FileService(account_name=FILE_ACCOUNT_NAME, account_key=FILE_ACCOUNT_KEY)
 
         onnx_file = file_service.get_file_to_bytes('models-share', 'mnist', 'mnist.onnx')
         
         print(f"Inference session initialized")
-        inferenceSession = rt.InferenceSession(onnx_file.content)
+        inference_session = rt.InferenceSession(onnx_file.content)
 
-    return inferenceSession
+    return inference_session
