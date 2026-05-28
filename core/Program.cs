eb282dcd9f5909b0bb229a99b6bb44cb32450c29
cvolcy <clervens.volcy@gmail.com>
2026-05-28 15:20:36 -0400

    enable httpclients

--- core/Program.cs
+++ core/Program.cs
@@ -2,6 +2,7 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
+        services.AddHttpClient();
     })
     .Build();
 
