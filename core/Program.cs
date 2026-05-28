8cf96b6f265e71ebc11c34fd19d0caba13a06cde
cvolcy <clervens.volcy@gmail.com>
2026-05-28 09:38:53 -0400

    feat: add Program.cs for isolated worker configuration

---
+++ core/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetry();
+    })
+    .Build();
+
+host.Run();
