2610daaaf05a44b31548006acc209303f013842b
Clervens Volcy <clervens.volcy@centris.ca>
2026-06-05 20:07:43 -0400

    setup ai depedencies

--- core/Program.cs
+++ core/Program.cs
@@ -1,7 +1,19 @@
+using fast_everglades.Infrastructure.Configuration;
+using Microsoft.Extensions.AI;
+
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services => {
+    .ConfigureAppConfiguration(builder =>
+    {
+        var environmentName = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT");
+        builder
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+            .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: false)
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) => {
         services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureLLM(context.Configuration);
     })
     .Build();
 
